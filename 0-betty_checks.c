8a048fe364c14ca29f9b0fdb6199282a0f0b07c9
Bulelani Botman <69175349+blkniga@users.noreply.github.com>
2022-09-03 01:04:41 +0200

    Edited the addition and also the main function

--- 0-betty_checks.c
+++ 0-betty_checks.c
@@ -2,7 +2,8 @@
 
 /**
  * add - add integers
- *
+ * @a: first integer
+ * @b: second integer
  * Return: On success 0
  */
 
@@ -12,14 +13,24 @@ int add(int a, int b)
 
 	c = a + b;
 
-	return (0);
+	return (c);
 }
 
+/**
+ * main - Runs the addition function and shows the output
+ *
+ * Return: On success 0
+ */
 
 int main(void)
 {
 	int x = 5;
 	int y = 2;
-
-	add(x, y);
+	int result;
+	
+	result = add(x, y);
+	
+	printf("%d", result);
+	
+	return (0);
 }
