1d7de26ea240a1945d1bae1a66686cb3fc3bc19b
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-09-02 15:58:24 -0700

    Task 0

---
+++ 0-betty_checks.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * add - add integers
+ *
+ * Return: On success 0
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (0);
+}
+
+
+int main(void)
+{
+	int x = 5;
+	int y = 2;
+
+	add(x, y);
+}
