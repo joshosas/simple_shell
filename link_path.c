97ff4e6665af6ba93216626ee00d6a6590de0c6c
Bulelani Botman <69175349+blkniga@users.noreply.github.com>
2022-09-06 14:01:40 +0200

    added three functions

--- link_path.c
+++ link_path.c
@@ -4,7 +4,7 @@
 /**
  * _getenv - funtion gets value of global variable
  * @name: global variable name
- * Return: string value
+ * Return: string
  */
 char *_getenv(const char *name)
 {
@@ -42,7 +42,6 @@ char *_getenv(const char *name)
  * @str: pointer to string in previous node
  * Return: address of the new element
  */
-
 list_path *add_node_end(list_path **head, char *str)
 {
 
@@ -77,4 +76,71 @@ list_path *add_node_end(list_path **head, char *str)
 	}
 
 	return (*head);
+}
+
+/**
+ * linkpath - creates a linked list for path directories
+ * @path: string of path value
+ * Return: pointer to the created linked list
+ */
+list_path *linkpath(char *path)
+{
+	list_path *head = '\0';
+	char *token;
+	char *cpath = _strdup(path);
+
+	token = strtok(cpath, ":");
+	while (token)
+	{
+		head = add_node_end(&head, token);
+		token = strtok(NULL, ":");
+	}
+
+	return (head);
+}
+
+/**
+ * _which - finds the pathname of a filename
+ * @filename: name of file or command
+ * @head: head of linked list of path directories
+ * Return: pathname of filename or NULL if no match
+ */
+char *_which(char *filename, list_path *head)
+{
+	struct stat st;
+	char *string;
+
+	list_path *tmp = head;
+
+	while (tmp)
+	{
+
+		string = concat_all(tmp->dir, "/", filename);
+		if (stat(string, &st) == 0)
+		{
+			return (string);
+		}
+		free(string);
+		tmp = tmp->p;
+	}
+
+	return (NULL);
+}
+
+/**
+ * free_list - frees a list_t
+ * @head: pointer to our linked list
+ */
+void free_list(list_path *head)
+{
+	list_path *storage;
+
+	while (head)
+	{
+		storage = head->p;
+		free(head->dir);
+		free(head);
+		head = storage;
+	}
+
 }
