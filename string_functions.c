be594bf93b0d4a30df74ba061bc1700fb04d9628
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-09-06 05:29:00 -0700

    create string_functions file

---
+++ string_functions.c
@@ -0,0 +1,69 @@
+#include "shell.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: pointer to string
+ * Return: pointer to a string
+ */
+char *_strdup(char *str)
+{
+	int i, l;
+	char *new;
+
+	if (!str)
+	{
+		return (NULL);
+	}
+	for (l = 0; str[l] != '\0';)
+	{
+		l++;
+	}
+	new = malloc(sizeof(char) * l + 1);
+	if (!new)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < l; i++)
+	{
+		new[i] = str[i];
+	}
+	new[l] = str[l];
+	return (new);
+}
+
+/**
+ * concat_all - concats 3 strings in a newly allocated memory
+ * @name: 1st str
+ * @sep: 2nd str
+ * @value: 3rd str
+ * Return: pointer to the new string
+ */
+char *concat_all(char *name, char *sep, char *value)
+{
+	char *result;
+	int l1, l2, l3, i, k;
+
+	l1 = _strlen(name);
+	l2 = _strlen(sep);
+	l3 = _strlen(value);
+
+	result = malloc(l1 + l2 + l3 + 1);
+	if (!result)
+		return (NULL);
+
+	for (i = 0; name[i]; i++)
+		result[i] = name[i];
+	k = i;
+
+	for (i = 0; sep[i]; i++)
+		result[k + i] = sep[i];
+	k = k + i;
+
+	for (i = 0; value[i]; i++)
+		result[k + i] = value[i];
+	k = k + i;
+
+	result[k] = '\0';
+
+	return (result);
+}
