123b312b588aafab41063205fb47572e90715435
Bulelani Botman <69175349+blkniga@users.noreply.github.com>
2022-09-06 14:14:16 +0200

    Updated the file with two functions

--- builtin.c
+++ builtin.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * exitt - function exits the shell with/without a return of status
+ * exitt - function exits the shell with / without a return of status
  * @arv: array of words
  */
 void exitt(char **arv)
@@ -24,8 +24,8 @@ void exitt(char **arv)
 
 /**
  * _atoi - function converts string to integer
- *@s: pointer par
- *Return: int
+ * @s: pointer par
+ * Return: integer
  */
 int _atoi(char *s)
 {
@@ -50,7 +50,7 @@ int _atoi(char *s)
 }
 
 /**
- * env - functio prints current environment
+ * env - function prints current environment
  * @arv: array of args
  */
 void env(char **arv __attribute__ ((unused)))
@@ -64,4 +64,92 @@ void env(char **arv __attribute__ ((unused)))
 		_puts("\n");
 	}
 
+}
+
+/**
+ * _setenv - Initialize a new environment variable, or modify an existing one
+ * @arv: array of entered words
+ */
+void _setenv(char **arv)
+{
+	int i, j, k;
+
+	if (!arv[1] || !arv[2])
+	{
+		perror(_getenv("_"));
+		return;
+	}
+
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (arv[1][j] == environ[i][j])
+		{
+			while (arv[1][j])
+			{
+				if (arv[1][j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (arv[1][j] == '\0')
+			{
+				k = 0;
+				while (arv[2][k])
+				{
+					environ[i][j + 1 + k] = arv[2][k];
+					k++;
+				}
+				environ[i][j + 1 + k] = '\0';
+				return;
+			}
+		}
+	}
+	if (!environ[i])
+	{
+
+		environ[i] = concat_all(arv[1], "=", arv[2]);
+		environ[i + 1] = '\0';
+
+	}
+}
+
+/**
+ * _unsetenv - Remove an environment variable
+ * @arv: array of entered words
+ */
+void _unsetenv(char **arv)
+{
+	int i, j;
+
+	if (!arv[1])
+	{
+		perror(_getenv("_"));
+		return;
+	}
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (arv[1][j] == environ[i][j])
+		{
+			while (arv[1][j])
+			{
+				if (arv[1][j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (arv[1][j] == '\0')
+			{
+				free(environ[i]);
+				environ[i] = environ[i + 1];
+				while (environ[i])
+				{
+					environ[i] = environ[i + 1];
+					i++;
+				}
+				return;
+			}
+		}
+	}
 }
