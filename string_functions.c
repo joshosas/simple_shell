51eb7d4386e967db55eefd9faf0fb17d31aff7e4
Bulelani Botman <69175349+blkniga@users.noreply.github.com>
2022-09-06 14:30:21 +0200

    Added new functions

--- string_functions.c
+++ string_functions.c
@@ -67,3 +67,45 @@ char *concat_all(char *name, char *sep, char *value)
 
 	return (result);
 }
+
+/**
+ * _strlen - it gives the length of a string
+ * @s: pointer to the string
+ * Return: the length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - prints a string
+ * @str: pointer to string
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
